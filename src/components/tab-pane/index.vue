5fc623bee3fccc0e7b28a46feb32d274d42ac1ce
l1m2e <1956227985@qq.com>
2022-10-22 21:09:54 +0800

    first commit

---
+++ src/components/tab-pane/index.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+defineProps<{
+	name: string
+	tab: string
+}>()
+</script>
+
+<template>
+	<div>
+		<slot></slot>
+	</div>
+</template>
+
+<style lang="scss"></style>
--- src/pages/rent-classroom/index.vue
+++ src/pages/rent-classroom/index.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
-import card from './components/card.vue'
-import { getRentListApi } from '@/api'
+// import card from './components/card.vue'
+// import { getRentListApi } from '@/api'
+import Tabs from '@/components/tabs/index.vue'
+import TabPane from '@/components/tab-pane/index.vue'
 definePageConfig({
 	navigationBarTitleText: '借用',
 	navigationBarBackgroundColor: '#fafafa'
@@ -11,60 +13,60 @@ const addRent = () => {
 	})
 }
 
-const nowLit = ref<any[]>([])
-const futureList = ref<any[]>([])
-const historyList = ref<any[]>([])
+// const nowLit = ref<any[]>([])
+// const futureList = ref<any[]>([])
+// const historyList = ref<any[]>([])
 
-const getRentList = async (type: string, length: number = 0) => {
-	const param = {
-		action: 1,
-		indexStart: length,
-		indexEnd: 10 + length,
-		timeType: type,
-		interfaceNum: '39-13'
-	}
-	const res = await getRentListApi(param)
-	if (res.statusCode === 200) {
-		console.log('[ res.data ] >', res.data)
-		switch (type) {
-			case 'now':
-				res.data.list.forEach((item: any) => {
-					nowLit.value.push(item)
-				})
-				break
-			case 'future':
-				res.data.list.forEach((item: any) => {
-					futureList.value.push(item)
-				})
-				break
-			case 'past':
-				res.data.list.forEach((item: any) => {
-					historyList.value.push(item)
-				})
-				break
-		}
-	}
-}
-getRentList('now')
-getRentList('future')
-getRentList('past')
-const handleScroll = (e: any) => {
-	const { scrollTop, clientHeight, scrollHeight } = e.target
-	if (scrollTop + clientHeight === scrollHeight) {
-		console.log('滚动到底部啦')
-	}
-}
-const current1 = ref(0)
-const tabList1 = [{ title: '标签页1' }, { title: '标签页2' }, { title: '标签页3' }]
-const handleClick = (value) => {
-	current1.value = value
-}
+// const getRentList = async (type: string, length: number = 0) => {
+// 	const param = {
+// 		action: 1,
+// 		indexStart: length,
+// 		indexEnd: 10 + length,
+// 		timeType: type,
+// 		interfaceNum: '39-13'
+// 	}
+// 	const res = await getRentListApi(param)
+// 	if (res.statusCode === 200) {
+// 		console.log('[ res.data ] >', res.data)
+// 		switch (type) {
+// 			case 'now':
+// 				res.data.list.forEach((item: any) => {
+// 					nowLit.value.push(item)
+// 				})
+// 				break
+// 			case 'future':
+// 				res.data.list.forEach((item: any) => {
+// 					futureList.value.push(item)
+// 				})
+// 				break
+// 			case 'past':
+// 				res.data.list.forEach((item: any) => {
+// 					historyList.value.push(item)
+// 				})
+// 				break
+// 		}
+// 	}
+// }
+// getRentList('now')
+// getRentList('future')
+// getRentList('past')
+// const handleScroll = (e: any) => {
+// 	const { scrollTop, clientHeight, scrollHeight } = e.target
+// 	if (scrollTop + clientHeight === scrollHeight) {
+// 		console.log('滚动到底部啦')
+// 	}
+// }
+// const current1 = ref(0)
+// const tabList1 = [{ title: '标签页1' }, { title: '标签页2' }, { title: '标签页3' }]
+// const handleClick = (value) => {
+// 	current1.value = value
+// }
 </script>
 
 <template>
 	<div class="rent-classroom">
 		<div class="addBtn" @click="addRent"><div class="i-ri-add-line  color-white text-25px"></div></div>
-		<AtTabs :swipeable="false" :current="current1" :tabList="tabList1" @click="handleClick">
+		<!-- <AtTabs :swipeable="false" :current="current1" :tabList="tabList1" @click="handleClick">
 			<AtTabsPane :current="current1" :index="0">
 				<view class="tab-content">标签页一的内容</view>
 			</AtTabsPane>
@@ -74,7 +76,13 @@ const handleClick = (value) => {
 			<AtTabsPane :current="current1" :index="2">
 				<view class="tab-content">标签页三的内容</view>
 			</AtTabsPane>
-		</AtTabs>
+		</AtTabs> -->
+		<Tabs name="123" default="tab2">
+			<TabPane name="tab1" tab="tab1">tab1</TabPane>
+			<TabPane name="tab2" tab="tab2">tab2</TabPane>
+			<TabPane name="tab3" tab="tab3">tab3</TabPane>
+			<TabPane name="tab4" tab="tab4">tab4</TabPane>
+		</Tabs>
 	</div>
 </template>
 
